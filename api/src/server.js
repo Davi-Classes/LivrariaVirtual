2a9298ef8feaeeb150a673ac242b8811120bab02
Davi Lucciola <davilucciola14@gmail.com>
2024-06-06 21:11:20 -0300

    api

---
+++ api/src/server.js
@@ -0,0 +1,76 @@
+import express from "express";
+
+const db = [
+  {
+    titulo: "Ultra Aprendizado",
+    autor: "Scott H. Young",
+    ano: 2019,
+    editora: "Moderno",
+  },
+  {
+    titulo: "Harry Potter - A pedra filosofal",
+    autor: "J. K. Rowlling",
+    ano: 1999,
+    editora: "Moderno",
+  },
+];
+
+const app = express();
+app.use(express.json());
+
+// Rota para Listar os Livros
+app.get("/livros", (req, res) => {
+  res.json(db);
+});
+
+// Rota para Cadastrar os Livros
+app.post("/livros", (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      message: "O campo titulo é obrigatório.",
+    });
+  }
+
+  if (!body.titulo) {
+    return res.status(400).json({
+      message: "O campo titulo é obrigatório.",
+    });
+  }
+
+  if (!body.autor) {
+    return res.status(400).json({
+      message: "O campo autor é obrigatório.",
+    });
+  }
+
+  console.log(body)
+  if (!body.ano) {
+    return res.status(400).json({
+      message: "O campo ano é obrigatório.",
+    });
+  }
+
+  const ano = Number(ano);
+
+  if (isNaN(ano)) {
+    return res.status(400).json({
+      message: "O campo ano deve ser um valor inteiro.",
+    });
+  }
+
+  const novoLivro = {
+    titulo: body.titulo,
+    autor: body.autor,
+    ano: body.autor,
+    editora: body.editora,
+  };
+
+  db.push(novoLivro);
+  res.status(201).json({
+    message: "Livro cadastrado com sucesso.",
+  });
+});
+
+app.listen(3000, () => console.log("Servidor rodando na porta 3000."));
